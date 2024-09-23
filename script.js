add98b67419e9584c90ed61ca51e994047f5b70b
Or Barer <or.barer@gmail.com>
2024-09-23 11:53:38 +0300

    changed the way the logic for storing the numbers and populating the display works

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function divide(a, b) {
 
 function operate() {
   if (stage == 3) {
+    b = Number(display.innerText);
     switch (op) {
       case "+":
         a = add(a, b);
@@ -41,6 +42,7 @@ function operate() {
         a = Math.round(divide(a, b) * 1000) / 1000;
         break;
     }
+    afterCalc = true;
     b = 0;
     stage = 2;
     if (a == 69) display.innerText = String(a) + " - matty sexy yes ;)";
@@ -49,20 +51,22 @@ function operate() {
 }
 
 function number(event) {
-  let input = Number(event.target.innerText);
-  if (stage == 1) {
-    a = a * 10 + input;
-    display.innerText = String(a);
-  } else {
+  if (afterCalc) clearCalc();
+  if (stage == 2) {
+    display.innerText = "";
     stage = 3;
-    b = b * 10 + input;
-    display.innerText = String(b);
   }
+  if (display.innerText != "0")
+    display.innerText = display.innerText.concat(event.target.innerText);
+  else display.innerText = event.target.innerText;
 }
 
 function operation(event) {
-  if (stage == 3) operate();
-  stage = 2;
+  if (stage == 1) {
+    a = Number(display.innerText);
+    stage = 2;
+  } else if (stage == 3) operate();
+  afterCalc = false;
   op = event.target.innerText;
 }
 
@@ -72,6 +76,8 @@ function clearCalc() {
   b = 0;
   stage = 1;
   display.innerText = "0";
+  dotted = false;
+  afterCalc = false;
 }
 
 function dot() {
@@ -85,4 +91,5 @@ let op = null;
 let b = 0;
 let stage = 1;
 let dotted = false;
+let afterCalc = false;
 let display = document.querySelector("#display");
